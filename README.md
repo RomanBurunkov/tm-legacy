cad6356c90c0cce813d1361cfdd4908ddde8e50f
Roman <romon2002@gmail.com>
2021-03-29 23:18:06 +0300

    Add tests and readme for validate method

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 [![codecov](https://codecov.io/gh/RomanBurunkov/tm-legacy/branch/main/graph/badge.svg?token=DXV24TPWYP)](https://codecov.io/gh/RomanBurunkov/tm-legacy)
 
 Get, save and update backup settings.
+
+### Installation
+
+```
+npm i tm-legacy
+```
+
+### Options
+
+  Options is an object with the following fields:
+
+  - `file` File name to store/read backup settings.
+  - `path` Path to store/read backup settings file.
+  - `keys` Array with backup settings names(Settings object fields names).
+
+### Methods
+
+  - `validate` Checks whether backup settings is available.
--- test/lib/legacy.spec.js
+++ test/lib/legacy.spec.js
@@ -4,12 +4,30 @@ const path = require('path');
 const TMLegacy = require('../../lib/legacy');
 
 const file = 'test.legacy.json';
-const legacyKeys = ['option1', 'option2'];
+const keys = ['option1', 'option2'];
 const mockLegacy = () => ({
   option1: 'val1',
   option2: 'val2',
 });
 
+const testLegacyPath = () => path.join(os.tmpdir(), file);
+
+const deleteTestLegacy = async () => {
+  try {
+    await fs.promises.unlink(testLegacyPath());
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return;
+    }
+    throw error;
+  }
+};
+
+const legacy = new TMLegacy({
+  file,
+  path: os.tmpdir(),
+  keys,
+});
 
 describe('Test TMLegacy class', () => {
   test('TMLegacy should be defined.', () => {
@@ -17,12 +35,22 @@ describe('Test TMLegacy class', () => {
   });
 
   test('TMLegacy.validate should return false if file does not exist', async () => {
-    const legacy = new TMLegacy({
-      file,
-      path: path.join(os.tmpdir(), file),
-      legacyKeys,
-    });
+    await deleteTestLegacy();
     const valid = await legacy.validate();
     expect(valid).toBe(false);
+  });
+
+  test('TMLegacy.validate should return false if file does not contains json', async () => {
+    await fs.promises.writeFile(testLegacyPath(), 'Some data');
+    const valid = await legacy.validate();
+    await deleteTestLegacy();
+    expect(valid).toBe(false);
+  });
+
+  test('TMLegacy.validate should return true if file exist and contains json', async () => {
+    await fs.promises.writeFile(testLegacyPath(), JSON.stringify(mockLegacy()));
+    const valid = await legacy.validate();
+    await deleteTestLegacy();
+    expect(valid).toBe(true);
   });
 });
