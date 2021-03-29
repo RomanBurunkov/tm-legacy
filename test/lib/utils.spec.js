ba934a2b96f83a2880d94af387b465d887ff8a26
Roman <romon2002@gmail.com>
2021-03-29 15:59:50 +0300

    Add basic utils read/save json tests

--- test/lib/utils.spec.js
+++ test/lib/utils.spec.js
@@ -1,9 +1,37 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 const utils = require('../../lib/utils');
+const { isFunc } = require('tm-is');
 
-describe('Test utils functions', () => {
+const mockFile = path.join(os.tmpdir(), 'legacy.test.json');
+const mockObj = { a: 1, b: 2, c: 'test' };
+
+describe('Test utils functions defined', () => {
   ['readJson', 'saveJson', 'logMsg', 'logError'].forEach((f) => {
-    test(`Function ${f} defined`, () => {
+    test(`Function ${f} should be defined`, () => {
       expect(utils[f]).toBeDefined();
     });
+
+    test(`${f} should be a function`, () => {
+      expect(isFunc(utils[f])).toBe(true);
+    });
+  });
+});
+
+describe('Test saveJson', () => {
+  test('saveJson should save an object to a file', async () => {
+    await utils.saveJson(mockFile, mockObj);
+    await fs.promises.stat(mockFile);
+    return fs.promises.unlink(mockFile);
+  });
+});
+
+describe('Test readJson', () => {
+  test('saveJson should get an object from a file', async () => {
+    await utils.saveJson(mockFile, mockObj);
+    const data = await utils.readJson(mockFile);
+    fs.promises.unlink(mockFile);
+    expect(data).toEqual(mockObj);
   });
 });
